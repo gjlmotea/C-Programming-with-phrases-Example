c23b54773b6a601b74521008be6cb505c5d53111
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-06-04 00:47:09 +0800

    hw_3-08
    hw_3-08

--- ch03_運算/hw_3-08.c
+++ ch03_運算/hw_3-08.c
@@ -3,6 +3,9 @@
 int main() {
 	int a = 0, b = 0, c = 0;
 	scanf("%d%d%d", &a, &b, &c);
-	
+	// (-b±√(b^2-4ac)) / 2a
+	//Multiple roots: b^2 - 4ac = 0
+	printf("%d", b * b == 4 * a * c);
+		
 	return 0;
 }
