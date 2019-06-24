fd31f876ff35257e5480dc8e990b5b74b2d3a117
GJLMoTea <gjlmotea@gmail.com>
2019-06-24 16:36:49 +0800

    使用多層if判定閏年

---
+++ ch04_判斷/4-15.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int year, k;
+	scanf("%d", &year);
+	if ( year % 400 != 0 )
+		if ((year % 4 == 0 ) && (year % 100 != 0 ))
+			k = 1;
+		else
+			k = 0;
+	else
+		k = 1;
+	printf("%d\n", k);
+
+	return 0;
+}
