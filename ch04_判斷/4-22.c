d6537d0fcfd4f766c1a817aa8ea382e6358d1f5b
GJLMoTea <gjlmotea@gmail.com>
2019-06-24 18:18:51 +0800

    使用switch依據power值做不同的事

---
+++ ch04_判斷/4-22.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	int i, j, power;
+	scanf("%d %d", &power, &i);
+	switch(power){
+	case 1:
+		j = i;
+		break;
+	case 2:
+		j = i * i;
+		break;
+	case 3:
+		j = i * i * i;
+		break;
+	default:
+		j = 0;
+	}
+	printf("%d\n", j);
+
+	return 0;
+}
