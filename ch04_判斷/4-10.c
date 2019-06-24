c7e62da978c4bc7f315c98221eb085677a31cd2e
GJLMoTea <gjlmotea@gmail.com>
2019-06-24 16:22:53 +0800

    決定三數中的最大值

---
+++ ch04_判斷/4-10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int i, j, k, max;
+	scanf("%d %d %d", &i, &j, &k);
+	
+	if ( i > j)
+		max = i;
+	else
+		max = j;
+	if ( k > max)
+		max = k;
+	printf("%d\n", max);
+	
+	return 0;	
+}
