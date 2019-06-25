292fea3695179c382b6d49c9d5678bd5075669ff
GJLMoTea <gjlmotea@gmail.com>
2019-06-25 17:12:54 +0800

    決定一個月的天數

---
+++ ch04_判斷/4-24.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+	int year, month, days;
+	scanf("%d", &year);
+	scanf("%d", &month);
+	switch(month){
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			days = 31;
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			days = 30;
+			break;
+		case 2:
+			if((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))){
+				days = 29;
+			}else{
+				days = 28;
+			}
+			break;
+		default:
+			days = 0;
+	}
+	printf("%d\n", days);
+
+	return 0;
+}
