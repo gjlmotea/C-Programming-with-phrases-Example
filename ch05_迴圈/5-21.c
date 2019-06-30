8954c843b8df7569713d01edfd09978223fcba5c
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-06-30 11:57:11 +0800

    判斷是否為質數 用旗標值來提早結束一個for迴圈
    判斷是否為質數 用旗標值來提早結束一個for迴圈

--- ch05_迴圈/5-21.c
+++ ch05_迴圈/5-21.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-	int i, j = 0, n;
+	int i, j = 1, n;
 	scanf("%d", &n);
-	for(i = 2; (i * i <= n ) && (j == 0); i++)
+	for(i = 2; (i * i <= n ) && (j == 1); i++)
 		if(n % i == 0)
-			j = 1;
+			j = 0;
 	printf("%d\n", j);
 			
 	return 0;
