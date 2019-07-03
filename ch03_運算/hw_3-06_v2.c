b2ccf49fdcbffc69028eacc2ae66bef6451bf688
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-07-03 23:50:09 +0800

    hw_3-06_v2
    hw_3-06_v2

--- ch03_運算/hw_3-06_v2.c
+++ ch03_運算/hw_3-06_v2.c
@@ -4,7 +4,8 @@
 int main() {
 	int a=0, b=0, c=0, d=0;
 	scanf("%d %d %d %d", &a, &b, &c, &d);
-	printf("%d", (abs(c*b-a*d)+1 * ((int)pow(10,abs(c*b-a*c)))%10));
+	printf("%d", (abs(c*b-a*d)+1) * ((int)pow(10,abs(c*b-a*c)))%10);
+	//完美版
 	
 	return 0;
 }
