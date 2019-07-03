bdb6ec85498821e7f548336b53e5932b5befe0e2
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-07-03 23:45:16 +0800

    hw_3-06_v2
    hw_3-06_v2

---
+++ ch03_運算/hw_3-06_v2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int a=0, b=0, c=0, d=0;
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	printf("%d", (abs(c*b-a*d)+1 * ((int)pow(10,abs(c*b-a*c)))%10));
+	
+	return 0;
+}
