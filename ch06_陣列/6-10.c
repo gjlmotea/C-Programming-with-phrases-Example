caedbed69f42538ef72696486b4dc7fcc217f4cc
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-07-10 00:27:29 +0800

    印出陣列a中元素的大小及地址
    印出陣列a中元素的大小及地址

--- ch06_陣列/6-10.c
+++ ch06_陣列/6-10.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-	long int a[10];
-	long int i;
+	int a[10];
+	int i;
 	
 	printf("%d\n", sizeof(a[0]));
 	printf("%d\n", sizeof(a));
 	for(i = 0; i < 10; i++)
-		printf("%d\n", &a[i]);
+		printf("%p\n", &a[i]);
 	printf("%p\n", &a);
 	printf("%p\n", a);
 	
