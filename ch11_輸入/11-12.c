e4abc8a11a2b8c358654776e2c6ee7bbb085f238
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-07-10 02:31:11 +0800

    三種不同方式宣告的字串sizeof
    三種不同方式宣告的字串sizeof

--- ch11_輸入/11-12.c
+++ ch11_輸入/11-12.c
@@ -6,9 +6,10 @@ int main() {
 	char str2[] = "programming";
 	char *str3 = "programming";
 	
-	printf("sizeof(str1) = %d\n", sizeof(str1));
-	printf("sizeof(str2) = %d\n", sizeof(str2));
-	printf("sizeof(str3) = %d\n", sizeof(str3));
+	printf("sizeof(str1) = %d\n", sizeof(str1));	//本身宣告了80個bytes，所以佔了80個bytes
+	printf("sizeof(str2) = %d\n", sizeof(str2));	//沒說長度，編譯器算出來長度為12 bytes，因為最後還需要儲存一個'\0' 
+	printf("sizeof(str3) = %d\n", sizeof(str3));	//*str3 本身 只佔4個bytes，編譯器會在唯讀記憶體中放一個字元陣列
+													//初始化為"programming" ，再將 *str3指向這個字元陣列 
 	
 	return 0;
 }
