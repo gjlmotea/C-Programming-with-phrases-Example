2a9f36b4ae3ffe891b115dcd518908e17528e28a
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-07-10 02:48:37 +0800

    strcpy (string copy)
    strcpy (string copy)

---
+++ ch11_輸入/11-19.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char source[100];
+	char destination[100];
+	
+	scanf("%s", source);
+	scanf("%s", destination);
+	
+	printf("%s\n", destination);
+	strcpy(destination, source);
+	printf("%s\n", destination);
+	
+	return 0;
+}
