def7dfc6a911a6769605979139d09f8ffd86860d
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-07-09 22:31:11 +0800

    印出n之內的質數
    印出n之內的質數

--- ch06_陣列/6-07.c
+++ ch06_陣列/6-07.c
@@ -7,10 +7,12 @@ int main() {
 	for(i = 2; i <= n; i++)
 		composite[i] = 0;
 	while(j * j <= n){
-		while(composite[j] == 1)
+		if(composite[j] == 1)
 			j++;
 		for(i = 2 * j; i <= n; i += j)
 			composite[i] = 1;
+			//j的倍數皆為合成數，composite[2*j] composite[3*j] composite[4*j] ...等都設為1，
+			//代表2*j、3*j、4*j...都是合成數 
 		j++;
 	}
 	for(i = 2; i <= n; i++)
