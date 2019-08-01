e67b7636799f6e5084d6559e7171d3075127f702
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-08-02 00:05:48 +0800

    hw_4-03
    hw_4-03

---
+++ ch04_判斷/hw_4-03.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	int a, b, c, d;
+	//point (a, b), (c, d)
+	int w = 0, h = 0;
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	if(d > b){
+		w = d - b;
+	}else{
+		w = b - d;
+	}
+	
+	if(c > a){
+		h = c - a;
+	}else{
+		c = a - c;
+	}
+	printf("%d", h * w);
+	
+	return 0;
+}
